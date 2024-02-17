4af36525bb738d0949196d281b527463a08250b4
kero0 <kbakheat@gmail.com>
2024-02-17 12:58:31 -0500

    Update last rotation and add dir-locals compile command

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((compile-command . "rm -f resume.pdf; cp \"$(nix build --no-link --print-out-paths)/resume.pdf\" ./"))))
